51f0da2ebd6a54e249eda8aa27931ca477ac2582
speci <speci@LAPTOP-HUHMAAUK>
2021-03-31 19:28:52 -0700

    Completed Chapter 8

---
+++ OutdoorProducts/Infrastructure/UrlExtentions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace OutdoorProducts.Infrastructure
+{
+    public static class UrlExtentions
+    {
+        public static string PathAndQuery(this HttpRequest request) =>
+             request.QueryString.HasValue
+                ? $"{request.Path}{request.QueryString}"
+                : request.Path.ToString();
+    }
+}
+
